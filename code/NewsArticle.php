77531609496777e353fbf2835891d3f8704e7937
helpfulrobot <chris@silverstripe.com>
2015-12-18 09:07:16 +1300

    Converted to PSR-2

--- code/NewsArticle.php
+++ code/NewsArticle.php
@@ -9,108 +9,116 @@
  * Authors: Techno Joy development team (www.technojoy.co.nz)
  */
 
-class NewsArticle extends DataObject {
+class NewsArticle extends DataObject
+{
 
-	public static $rss_thumb_width = 250;
+    public static $rss_thumb_width = 250;
 
-	public static $default_sort = array(
-		'"Date"' => 'DESC',
-		'"ID"' => 'DESC'
-	);
+    public static $default_sort = array(
+        '"Date"' => 'DESC',
+        '"ID"' => 'DESC'
+    );
 
-	private static $db = array(
-		'Title' => 'Varchar(200)',
-		'Date' => 'Datetime',
-		'Content' => 'HTMLText'
-	);
+    private static $db = array(
+        'Title' => 'Varchar(200)',
+        'Date' => 'Datetime',
+        'Content' => 'HTMLText'
+    );
 
-	private static $has_one = array(
-		'Parent' => 'SiteTree',
-		'Thumbnail' => 'Image'
-	);
+    private static $has_one = array(
+        'Parent' => 'SiteTree',
+        'Thumbnail' => 'Image'
+    );
 
-	public static $summary_fields = array(
-		'Date',
-		'Title'
-	);
+    public static $summary_fields = array(
+        'Date',
+        'Title'
+    );
 
-	public function getCmsFields() {
+    public function getCmsFields()
+    {
+        $fields = parent::getCMSFields();
 
-		$fields = parent::getCMSFields();
+        $fields->removeByName('ParentID');
 
-		$fields->removeByName('ParentID');
+        // Set today's date if empty
+        if (!$this->Date) {
+            $this->Date = date('Y-m-d H:i:s');
+        }
 
-		// Set today's date if empty
-		if (!$this->Date) {
-			$this->Date = date('Y-m-d H:i:s');
-		}
+        $fields->dataFieldByName('Title')->setTitle('Article Title');
 
-		$fields->dataFieldByName('Title')->setTitle('Article Title');
+        $fields->addFieldToTab('Root.Main',
+            LiteralField::create('DateInfo', '<p>Dates set in the future will not be visible until that day/time.</p>'),
+            'Date'
+        );
 
-		$fields->addFieldToTab('Root.Main',
-			LiteralField::create('DateInfo', '<p>Dates set in the future will not be visible until that day/time.</p>'),
-			'Date'
-		);
+        $fields->addFieldToTab('Root.Main',
+            $datetime = DatetimeField::create('Date', 'Article Date / Time (24h)'),
+            'Content'
+        );
+        $datetime->getDateField()->setConfig('showcalendar', 1);
+        $datetime->getTimeField()->setConfig('timeformat', 'HH:mm');
 
-		$fields->addFieldToTab('Root.Main',
-			$datetime = DatetimeField::create('Date', 'Article Date / Time (24h)'),
-			'Content'
-		);
-		$datetime->getDateField()->setConfig('showcalendar', 1);
-		$datetime->getTimeField()->setConfig('timeformat', 'HH:mm');
+        $fields->addFieldToTab('Root.Main',
+            $ul = UploadField::create('Thumbnail', 'Article Image'),
+            'Content'
+        );
+        $ul->setFolderName('NewsArticles');
+        $ul->setAllowedFileCategories('image');
 
-		$fields->addFieldToTab('Root.Main',
-			$ul = UploadField::create('Thumbnail', 'Article Image'),
-			'Content'
-		);
-		$ul->setFolderName('NewsArticles');
-		$ul->setAllowedFileCategories('image');
+        return $fields;
+    }
 
-		return $fields;
-	}
+    /**
+     * Generate RSS Content and allow extending
+     */
+    public function RssContent()
+    {
+        $thumbnail = false;
+        $t = $this->Thumbnail();
+        if ($t->ID) {
+            $thumbnail = '<p><a href="' . $t->URL . '"><img src="' . $t->setWidth(self::$rss_thumb_width)->URL .
+                '" alt="'. htmlspecialchars($t->Title) . '" /></a></p>';
+        }
+        $html = DBField::create_field(
+            'HTMLText',
+            $thumbnail . $this->Content
+        );
+        $this->extend('updateRssContent', $html);
+        return $html;
+    }
 
-	/**
-	 * Generate RSS Content and allow extending
-	 */
-	public function RssContent() {
-		$thumbnail = false;
-		$t = $this->Thumbnail();
-		if ($t->ID) {
-			$thumbnail = '<p><a href="' . $t->URL . '"><img src="' . $t->setWidth(self::$rss_thumb_width)->URL .
-				'" alt="'. htmlspecialchars($t->Title) . '" /></a></p>';
-		}
-		$html = DBField::create_field(
-			'HTMLText',
-			$thumbnail . $this->Content
-		);
-		$this->extend('updateRssContent', $html);
-		return $html;
-	}
+    /* Create the item link */
+    public function Link()
+    {
+        return $this->Parent()->Link() . 'article/' . $this->Parent()->generateURLSegment($this->Title) . '-' . $this->ID . '/';
+    }
 
-	/* Create the item link */
-	public function Link() {
-		return $this->Parent()->Link() . 'article/' . $this->Parent()->generateURLSegment($this->Title) . '-' . $this->ID . '/';
-	}
+    public function validate()
+    {
+        $valid = parent::validate();
+        if (trim($this->Title) == '') {
+            $valid->error('Please give your article a title');
+        }
+        return $valid;
+    }
 
-	public function validate() {
-		$valid = parent::validate();
-		if (trim($this->Title) == '')
-			$valid->error('Please give your article a title');
-		return $valid;
-	}
-
-	/* Permissions */
-	public function canView($member = null) {
-		return Permission::check('CMS_ACCESS_CMSMain', 'any', $member);
-	}
-	public function canEdit($member = null) {
-		return Permission::check('CMS_ACCESS_CMSMain', 'any', $member);
-	}
-	public function canCreate($member = null) {
-		return Permission::check('CMS_ACCESS_CMSMain', 'any', $member);
-	}
-	public function canDelete($member = null) {
-		return Permission::check('CMS_ACCESS_CMSMain', 'any', $member);
-	}
-
+    /* Permissions */
+    public function canView($member = null)
+    {
+        return Permission::check('CMS_ACCESS_CMSMain', 'any', $member);
+    }
+    public function canEdit($member = null)
+    {
+        return Permission::check('CMS_ACCESS_CMSMain', 'any', $member);
+    }
+    public function canCreate($member = null)
+    {
+        return Permission::check('CMS_ACCESS_CMSMain', 'any', $member);
+    }
+    public function canDelete($member = null)
+    {
+        return Permission::check('CMS_ACCESS_CMSMain', 'any', $member);
+    }
 }
--- code/NewsPage.php
+++ code/NewsPage.php
@@ -9,146 +9,157 @@
  * Authors: Techno Joy development team (www.technojoy.co.nz)
  */
 
-class NewsPage extends Page {
+class NewsPage extends Page
+{
 
-	public static $articles_per_page = 10;
+    public static $articles_per_page = 10;
 
-	public static $icon = 'silverstripe-news/images/news.png';
+    public static $icon = 'silverstripe-news/images/news.png';
 
-	public static $description = 'News page with articles';
+    public static $description = 'News page with articles';
 
-	private static $db = array();
+    private static $db = array();
 
-	private static $has_many = array(
-		'NewsArticles' => 'NewsArticle'
-	);
+    private static $has_many = array(
+        'NewsArticles' => 'NewsArticle'
+    );
 
-	public function getCmsFields() {
-		$fields = parent::getCMSFields();
-		$fields->removeByName('Content');
-		$gridField = new GridField('NewsItems', false, $this->NewsArticles(),
-			new GridFieldConfig_RecordEditor(self::$articles_per_page)
-		);
-		$fields->addFieldToTab('Root.Main', $gridField);
-		return $fields;
-	}
+    public function getCmsFields()
+    {
+        $fields = parent::getCMSFields();
+        $fields->removeByName('Content');
+        $gridField = new GridField('NewsItems', false, $this->NewsArticles(),
+            new GridFieldConfig_RecordEditor(self::$articles_per_page)
+        );
+        $fields->addFieldToTab('Root.Main', $gridField);
+        return $fields;
+    }
 
-	public static function getArticlesPerPage() {
-		return self::$articles_per_page;
-	}
-
+    public static function getArticlesPerPage()
+    {
+        return self::$articles_per_page;
+    }
 }
 
-class NewsPage_Controller extends Page_Controller {
+class NewsPage_Controller extends Page_Controller
+{
 
-	private static $allowed_actions = array('ViewArticle', 'rss');
+    private static $allowed_actions = array('ViewArticle', 'rss');
 
-	public static $url_handlers = array(
-		'article//$ArticleSegment' => 'ViewArticle',
-	);
+    public static $url_handlers = array(
+        'article//$ArticleSegment' => 'ViewArticle',
+    );
 
-	public function init() {
-		RSSFeed::linkToFeed($this->Link() . 'rss');
-		parent::init();
-	}
+    public function init()
+    {
+        RSSFeed::linkToFeed($this->Link() . 'rss');
+        parent::init();
+    }
 
-	public function index($request) {
-		$this->PaginatedArticles = new PaginatedList($this->NewsArticles()->filter(array('Date:LessThan' => $this->cur_time())), $request);
-		$this->PaginatedArticles->setPageLength($this->getArticlesPerPage());
-		return $this;
-	}
+    public function index($request)
+    {
+        $this->PaginatedArticles = new PaginatedList($this->NewsArticles()->filter(array('Date:LessThan' => $this->cur_time())), $request);
+        $this->PaginatedArticles->setPageLength($this->getArticlesPerPage());
+        return $this;
+    }
 
-	public function ViewArticle($request){
-		$segment = $request->param('ArticleSegment');
+    public function ViewArticle($request)
+    {
+        $segment = $request->param('ArticleSegment');
 
-		/* Split the URL */
-		if (!preg_match('/^(.*)\-(\d+)$/', $segment, $matches)) {
-			return $this->httpError(404);
-		}
+        /* Split the URL */
+        if (!preg_match('/^(.*)\-(\d+)$/', $segment, $matches)) {
+            return $this->httpError(404);
+        }
 
-		$UrlName = $matches[1];
-		$id = $matches[2];
+        $UrlName = $matches[1];
+        $id = $matches[2];
 
-		$this->Article = $this->NewsArticles()->filter(array(
-			'ID' => $id,
-			'Date:LessThan' => $this->cur_time()
-		))->First();
+        $this->Article = $this->NewsArticles()->filter(array(
+            'ID' => $id,
+            'Date:LessThan' => $this->cur_time()
+        ))->First();
 
-		/* Article not found */
-		if (!$this->Article) {
-			return $this->httpError(404);
-		}
+        /* Article not found */
+        if (!$this->Article) {
+            return $this->httpError(404);
+        }
 
-		/* Article on different page? */
-		if ($this->Article->ParentID != $this->ID || $UrlName != $this->generateURLSegment($this->Article->Title)) {
-			return $this->redirect($this->Article->Link(), 301);
-		}
+        /* Article on different page? */
+        if ($this->Article->ParentID != $this->ID || $UrlName != $this->generateURLSegment($this->Article->Title)) {
+            return $this->redirect($this->Article->Link(), 301);
+        }
 
-		/**
-		 * Override MetaData
-		 * Only works if template calls the $Title individually and not through $MetaTags
-		 * Title not overwritten else $Breadcrumbs does not work
-		 */
-		$this->dataRecord->MetaTitle = '';
-		$this->dataRecord->MetaDescription = '';
-		$this->dataRecord->MetaKeywords = '';
+        /**
+         * Override MetaData
+         * Only works if template calls the $Title individually and not through $MetaTags
+         * Title not overwritten else $Breadcrumbs does not work
+         */
+        $this->dataRecord->MetaTitle = '';
+        $this->dataRecord->MetaDescription = '';
+        $this->dataRecord->MetaKeywords = '';
 
-		return $this->renderWith(array('Article_view','Page'));
-	}
+        return $this->renderWith(array('Article_view', 'Page'));
+    }
 
-	/**
-	 * Override BreadCrumbs to add Article to list
-	 */
-	public function Breadcrumbs($maxDepth = 20, $unlinked = false, $stopAtPageType = false, $showHidden = false) {
-		$page = $this->Owner;
-		$pages = array();
-		if ($this->Article) $pages[] = $this->Article;
-		while(
-			$page
- 			&& (!$maxDepth || count($pages) < $maxDepth)
- 			&& (!$stopAtPageType || $page->ClassName != $stopAtPageType)
- 		) {
-			if($showHidden || $page->ShowInMenus || ($page->ID == $this->ID)) {
-				$pages[] = $page;
-			}
-			$page = $page->Parent;
-		}
-		$template = new SSViewer('BreadcrumbsTemplate');
-		return $template->process($this->customise(new ArrayData(array(
-			'Pages' => new ArrayList(array_reverse($pages))
-		))));
-	}
+    /**
+     * Override BreadCrumbs to add Article to list
+     */
+    public function Breadcrumbs($maxDepth = 20, $unlinked = false, $stopAtPageType = false, $showHidden = false)
+    {
+        $page = $this->Owner;
+        $pages = array();
+        if ($this->Article) {
+            $pages[] = $this->Article;
+        }
+        while (
+            $page
+            && (!$maxDepth || count($pages) < $maxDepth)
+            && (!$stopAtPageType || $page->ClassName != $stopAtPageType)
+        ) {
+            if ($showHidden || $page->ShowInMenus || ($page->ID == $this->ID)) {
+                $pages[] = $page;
+            }
+            $page = $page->Parent;
+        }
+        $template = new SSViewer('BreadcrumbsTemplate');
+        return $template->process($this->customise(new ArrayData(array(
+            'Pages' => new ArrayList(array_reverse($pages))
+        ))));
+    }
 
-	/**
-	 * Generate current date to ensure posts in the future are not shown
-	 */
-	public function cur_time() {
-		return date('Y-m-d H:i:s');
-	}
+    /**
+     * Generate current date to ensure posts in the future are not shown
+     */
+    public function cur_time()
+    {
+        return date('Y-m-d H:i:s');
+    }
 
-	/**
-	 * Override Title to cater for DataObject
-	 */
-	public function Title() {
-		return $this->Article ? $this->Article->Title : $this->Title;
-	}
+    /**
+     * Override Title to cater for DataObject
+     */
+    public function Title()
+    {
+        return $this->Article ? $this->Article->Title : $this->Title;
+    }
 
-	/**
-	 * Generate a RSS feed of news page
-	 */
-	public function rss() {
-		$siteConfig = SiteConfig::current_site_config();
-		$rss = new RSSFeed(
-			$this->NewsArticles()->filter(array('Date:LessThan' => $this->cur_time())),
-			$this->Link(),
-			$siteConfig->Title,
-			null,
-			'Title',
-			'RssContent',
-			null,
-			'Created'
-		);
-		return $rss->outputToBrowser();
-	}
-
+    /**
+     * Generate a RSS feed of news page
+     */
+    public function rss()
+    {
+        $siteConfig = SiteConfig::current_site_config();
+        $rss = new RSSFeed(
+            $this->NewsArticles()->filter(array('Date:LessThan' => $this->cur_time())),
+            $this->Link(),
+            $siteConfig->Title,
+            null,
+            'Title',
+            'RssContent',
+            null,
+            'Created'
+        );
+        return $rss->outputToBrowser();
+    }
 }
